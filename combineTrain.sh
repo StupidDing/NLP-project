9b37d0c82239bbf38a76b39f79563eb0d381167a
LIN Jingyuan <jingyuan.lin@connect.ust.hk>
2024-04-29 20:01:03 +0800

    auto train

--- combineTrain.sh
+++ combineTrain.sh
@@ -8,8 +8,8 @@ do
   vec_dim_dm=$((vec_dim-i))
   echo "${vec_dim_dbow}, ${vec_dim_dm}"
   source /Users/Shared/Anaconda/anaconda3/bin/activate /opt/anaconda3/envs/nlp-proj-36
-  python ./paragraphvec/train.py start --data_file_name "${file_name}.csv" --num_epochs 1 --batch_size 32 --num_noise_words 2 --num_workers 8 --vec_dim $vec_dim_dbow --lr 1e-3
-  python ./paragraphvec/train.py start --data_file_name "${file_name}.csv" --num_epochs 1 --batch_size 32 --num_noise_words 2 --num_workers 8 --vec_dim $vec_dim_dm --lr 1e-3 --model_ver dm --context_size 10
+  python ./paragraphvec/train.py start --data_file_name "${file_name}.csv" --num_epochs 50 --batch_size 32 --num_noise_words 2 --num_workers 8 --vec_dim $vec_dim_dbow --lr 1e-3
+  python ./paragraphvec/train.py start --data_file_name "${file_name}.csv" --num_epochs 50 --batch_size 32 --num_noise_words 2 --num_workers 8 --vec_dim $vec_dim_dm --lr 1e-3 --model_ver dm --context_size 10
   python ./paragraphvec/export_vectors.py start --data_file_name "${file_name}.csv" --model_file_name "${file_name}_model.dbow_vecdim.${vec_dim_dbow}_intermediate.tar"
   python ./paragraphvec/export_vectors.py start --data_file_name "${file_name}.csv" --model_file_name "${file_name}_model.dm_vecdim.${vec_dim_dm}_intermediate.tar"
   source /Users/Shared/Anaconda/anaconda3/bin/activate /opt/anaconda3
